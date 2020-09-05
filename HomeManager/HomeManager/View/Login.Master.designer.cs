38daac6e68d3573f57ba7ba9b41ff1640185cdd7
PinyBazs <balazs.pinter97@gmail.com>
2020-09-05 02:34:31 +0200

    login view with stylesheet added

--- HomeManager/HomeManager/View/Login.Master.designer.cs
+++ HomeManager/HomeManager/View/Login.Master.designer.cs
@@ -42,21 +42,21 @@ namespace HomeManager.View
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
 
         /// <summary>
-        /// Button1 control.
+        /// login_btn control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Button login_btn;
 
         /// <summary>
-        /// Button2 control.
+        /// signup_btn control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
+        protected global::System.Web.UI.WebControls.Button signup_btn;
     }
 }
