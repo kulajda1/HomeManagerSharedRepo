c701c1264f47529fafdd8df8fe2f7cf4311c03ea
Herédi Dániel <heredi.daniel@mod.hu>
2020-09-07 14:11:20 +0200

    változtatás

--- HomeManager/HomeManager/Repos/AreaRepo.cs
+++ HomeManager/HomeManager/Repos/AreaRepo.cs
@@ -14,6 +14,7 @@ namespace HomeManager.Model
                 HomeManagerEntities db = new HomeManagerEntities();
                 db.Area.Add(area);
                 db.SaveChanges();
+                Console.WriteLine("dsadasdsad");
 
                 return area.Name + " sikeresen felvéve a területek közé!";
             }
